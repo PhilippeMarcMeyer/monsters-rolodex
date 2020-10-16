13707db347806987aa24474d0a8bd114b63380cc
Philippe MEYER <p.meyer@mappingcontrol.com>
2020-10-16 12:01:08 +0200

    sending build

---
+++ build/precache-manifest.58a8cc5bd5984a7be80c368f9838c77a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138942c3975fc5199552e5b3dba97f16",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e45ed5ff88b5a8905be5",
+    "url": "/monster-rolodex/static/css/main.d2d9fc55.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e45ed5ff88b5a8905be5",
+    "url": "/monster-rolodex/static/js/main.12e0ddda.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
